fc0fa57ea1e8cc2970111ef6f7f0f55fb2c7ad53
Yubo <you-2001-3-6@ezweb.ne.jp>
2020-10-13 14:17:22 +0900

    add sample returns

--- main.py
+++ main.py
@@ -2,11 +2,33 @@ import algebra as alg
 
 x = alg.matrix([[0, 1, 2], [3, 4, 5]])
 y = alg.matrix([[1, 2], [3, 4], [5, 6]])
-k = 2
 
-print(x.T)
-print(len(x))
+print(x.T, x.T.shape)
+# matrix([
+#          [0, 3]
+#          [1, 4]
+#          [2, 5]
+#        ])
+
 print(alg.zeros((3, 3)))
+#  (3, 2)
+
 print(alg.zeros_like(x))
+# matrix([
+#          [0, 0, 0]
+#          [0, 0, 0]
+#          [0, 0, 0]
+#        ])
+
 print(alg.ones((3, 3)))
+# matrix([
+#          [0, 0, 0]
+#          [0, 0, 0]
+#        ])
+
 print(alg.ones_like(x))
+# matrix([
+#          [1, 0, 0]
+#          [0, 1, 0]
+#          [0, 0, 1]
+#        ])
